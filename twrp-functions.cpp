170f1235ea03347d2f066b86c06d40e5b241576f
Darth9 <reyprimerjefe@gmail.com>
2018-12-12 23:43:54 +0000

    Add licensing information

--- twrp-functions.cpp
+++ twrp-functions.cpp
@@ -1,6 +1,10 @@
 /*
 	Copyright 2012 bigbiff/Dees_Troy TeamWin
 	This file is part of TWRP/TeamWin Recovery Project.
+
+	Copyright (C) 2018-2019 OrangeFox Recovery Project
+	This file is part of the OrangeFox Recovery Project.
+
 	TWRP is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
@@ -13,6 +17,7 @@
 
 	You should have received a copy of the GNU General Public License
 	along with TWRP.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 #include <stdio.h>
