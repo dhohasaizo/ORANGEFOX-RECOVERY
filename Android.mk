a75d9eef85d8a0a293eef9cf19cc12f9c3f5c834
Darth9 <reyprimerjefe@gmail.com>
2019-02-10 21:31:02 +0000

    Minor updates, including copyright notices

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2007 The Android Open Source Project
+# Copyright (C) 2018-2019 OrangeFox Recovery Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -46,7 +47,7 @@ ifneq ($(TW_DEVICE_VERSION),)
     export FOX_DEVICE_VERSION=$(TW_DEVICE_VERSION)
 else
     LOCAL_CFLAGS += -DTW_DEVICE_VERSION='"Unofficial"'
-    export FOX_DEVICE_VERSION=R8.1
+    export FOX_DEVICE_VERSION=R9.0
 endif
 
 DEVICE := $(subst omni_,,$(TARGET_PRODUCT))
--- twrp-functions.hpp
+++ twrp-functions.hpp
@@ -2,6 +2,9 @@
 	Copyright 2012 bigbiff/Dees_Troy TeamWin
 	This file is part of TWRP/TeamWin Recovery Project.
 
+  	Copyright (C) 2018-2019 OrangeFox Recovery Project
+ 	This file is part of the OrangeFox Recovery Project
+ 
 	TWRP is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
