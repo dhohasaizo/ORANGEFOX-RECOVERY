2262304894a72c8c6a5c0791711a3a403d3061e1
MrYacha <YachaGorobtsov@gmail.com>
2019-04-19 20:50:49 +0300

    Right way to save of_no_miui_features

--- data.cpp
+++ data.cpp
@@ -651,7 +651,12 @@ void DataManager::SetDefaultValues()
   mConst.SetValue(OF_STATUS_INDENT_RIGHT_S, OF_STATUS_INDENT_RIGHT);
   mConst.SetValue(OF_CLOCK_POS_S, OF_CLOCK_POS);
   mConst.SetValue(OF_ALLOW_DISABLE_NAVBAR_S, OF_ALLOW_DISABLE_NAVBAR);
-  mData.SetValue("of_no_miui_features", OF_DISABLE_MIUI_SPECIFIC_FEATURES);
+
+  mData.SetValue("of_no_miui_features", 0);
+  #if defined(OF_DISABLE_MIUI_SPECIFIC_FEATURES)
+    mData.SetValue("of_no_miui_features", 1);
+  #endif 
+  
   //[f/d]
  
 #ifndef TW_NO_HAPTICS
