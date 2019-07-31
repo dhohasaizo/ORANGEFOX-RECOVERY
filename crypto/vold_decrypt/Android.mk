f2fa330509414536594a4ce3a8bf7ef908314fa7
Darth9 <reyprimerjefe@gmail.com>
2019-07-31 22:38:38 +0100

    Apply more patches for wrappedkey FBE (Peter Cai omnirom gerritt 34091/34092/34096)

--- crypto/vold_decrypt/Android.mk
+++ crypto/vold_decrypt/Android.mk
@@ -39,7 +39,7 @@ ifeq ($(TW_INCLUDE_CRYPTO), true)
 
         include $(CLEAR_VARS)
         LOCAL_MODULE := init.recovery.vold_decrypt.rc
-        LOCAL_MODULE_TAGS := eng optional
+        LOCAL_MODULE_TAGS := eng
         LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 
         # Cannot send to TARGET_RECOVERY_ROOT_OUT since build system wipes init*.rc
@@ -77,7 +77,6 @@ ifeq ($(TW_INCLUDE_CRYPTO), true)
         LOCAL_MODULE := libvolddecrypt
         LOCAL_MODULE_TAGS := eng optional
         LOCAL_CFLAGS := -Wall -Wno-unused-function
-#  R10.x      LOCAL_ADDITIONAL_DEPENDENCIES += init.recovery.vold_decrypt.rc
         ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
             LOCAL_C_INCLUDES += external/stlport/stlport bionic bionic/libstdc++/include
         endif
